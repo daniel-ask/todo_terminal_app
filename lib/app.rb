5a0609d93b1c804b4b3470190ec2ee7942c6dda3
Daniel Ask <cdanielask@gmail.com>
2021-03-30 00:34:04 +1100

    Restructured app and app_spec

--- lib/app.rb
+++ lib/app.rb
@@ -9,19 +9,23 @@ class App
     @tasks << { task: task_input, completed: false }
   end
 
+  def change_task(edited_task, index)
+    @tasks[index][:task] = edited_task
+  end
+
   def display_add_task
     puts 'Enter name of task'
   end
 
-  def task_add
-    gets.strip
-  end
-
   def display_tasks
     puts 'Tasks:'
-    @tasks.each_with_index do |task,index|
+    @tasks.each_with_index do |task, index|
       puts "#{index + 1}. #{task[:task]} [#{task[:completed] ? 'X' : ' '}]"
     end
+  end
+
+  def display_new_task
+    puts 'Please enter new task'
   end
 
   def display_select_task
@@ -32,11 +36,7 @@ class App
     gets.to_i - 1
   end
 
-  def display_new_task
-    puts 'Please enter new task'
-  end
-
-  def change_task(edited_task, index)
-    @tasks[index][:task] = edited_task
+  def task_add
+    gets.strip
   end
 end
--- spec/app_spec.rb
+++ spec/app_spec.rb
@@ -1,72 +1,33 @@
 require_relative '../lib/app'
 
+# Clearing ARGV to make rspec -f d to work
+ARGV.clear
+
 RSpec.describe App do
-  subject(:app){
+  subject(:app) do
     described_class.new
-  }
-
-  it 'should be an instance of App' do
-    expect(app).to be_a App
   end
 
-  it 'should have an empty task list' do
-    expect(app.tasks).to eq []
+  describe 'Class testing' do
+    it 'should be an instance of App' do
+      expect(app).to be_a App
+    end
+
+    it 'should have an empty task list' do
+      expect(app.tasks).to eq []
+    end
   end
 
-  context 'adding a task' do
+  describe '#add_task' do
     it 'should be able to add a task' do
       app.add_task('Create a todo app')
       expect(app.tasks.last).to eq({ task: 'Create a todo app', completed: false })
     end
-
-    it 'should ask user for input' do
-      expected_output = "Enter name of task\n"
-      expect{ app.display_add_task }.to output(expected_output).to_stdout
-    end
-
-  let(:input) { StringIO.new('test task') }
-    it 'should be able to receive task name from terminal' do
-      $stdin = input
-      expect(app.task_add).to eq('test task')
-    end
   end
 
-  context 'displaying tasks' do
+  describe '#change_task' do
     before(:each) do
-      app.tasks = [{task: 'test task', completed: false}]
-    end
-
-    it 'should display all our tasks' do
-      expected_output = "Tasks:\n1. test task [ ]\n"
-      expect{ app.display_tasks }.to output(expected_output).to_stdout
-    end
-
-    it 'should display tasks that are completed with an X' do
-      app.tasks << { task: 'test task 2', completed: true }
-      expected_output = "Tasks:\n1. test task [ ]\n2. test task 2 [X]\n"
-      expect{ app.display_tasks }.to output(expected_output).to_stdout
-    end
-
-    it 'should display task that are not completed with an space' do
-      app.tasks << { task: 'test task 2', completed: false }
-      expected_output = "Tasks:\n1. test task [ ]\n2. test task 2 [ ]\n"
-      expect{ app.display_tasks }.to output(expected_output).to_stdout
-    end
-  end
-
-  context 'edit task' do
-    before(:each) do
-      app.tasks = [{task: 'test task', completed: false}]
-    end
-
-    it 'should display a message to user to enter a task number' do
-      expected_output = "Please enter the number of the task:\n"
-      expect{ app.display_select_task }.to output(expected_output).to_stdout
-    end
-
-    it 'should ask user for a new task' do
-      expected_output = "Please enter new task\n"
-      expect{ app.display_new_task }.to output(expected_output).to_stdout
+      app.tasks = [{ task: 'test task', completed: false }]
     end
 
     it 'should change a task depending on the index' do
@@ -75,17 +36,69 @@ RSpec.describe App do
     end
 
     it 'should change a task if the index is 1' do
-      app.tasks << {task: 'This is task at index 1', completed: false}
+      app.tasks << { task: 'This is task at index 1', completed: false }
       app.change_task('This is the new task at index 1', 1)
       expect(app.tasks[1][:task]).to eq 'This is the new task at index 1'
     end
+  end
 
-    describe '#select_task' do
-      let(:input) { StringIO.new('1') }
-      it 'should return the index of task selected' do
-        $stdin = input
-        expect(app.select_task).to eq(0)
+  describe 'display methods' do
+    context '#display_add_task' do
+      it 'should ask user for input' do
+        expected_output = "Enter name of task\n"
+        expect { app.display_add_task }.to output(expected_output).to_stdout
       end
+    end
+
+    context '#display_select_task' do
+      it 'should display a message to user to enter a task number' do
+        expected_output = "Please enter the number of the task:\n"
+        expect { app.display_select_task }.to output(expected_output).to_stdout
+      end
+    end
+
+    context '#display_new_task' do
+      it 'should ask user for a new task' do
+        expected_output = "Please enter new task\n"
+        expect { app.display_new_task }.to output(expected_output).to_stdout
+      end
+    end
+
+    context '#display_tasks' do
+      before(:each) do
+        app.tasks = [{ task: 'test task', completed: false }]
+      end
+
+      it 'should display all our tasks' do
+        expected_output = "Tasks:\n1. test task [ ]\n"
+        expect { app.display_tasks }.to output(expected_output).to_stdout
+      end
+
+      it 'should display tasks that are completed with an X' do
+        app.tasks << { task: 'test task 2', completed: true }
+        expected_output = "Tasks:\n1. test task [ ]\n2. test task 2 [X]\n"
+        expect { app.display_tasks }.to output(expected_output).to_stdout
+      end
+
+      it 'should display task that are not completed with an space' do
+        app.tasks << { task: 'test task 2', completed: false }
+        expected_output = "Tasks:\n1. test task [ ]\n2. test task 2 [ ]\n"
+        expect { app.display_tasks }.to output(expected_output).to_stdout
+      end
+    end
+  end
+
+  describe 'input methods' do
+    let(:input) { StringIO.new('test task') }
+    it 'should be able to receive task name from terminal' do
+      $stdin = input
+      expect(app.task_add).to eq('test task')
+    end
+
+    let(:input) { StringIO.new('1') }
+    it 'should return the index of task selected' do
+      $stdin = input
+      expect(app.select_task).to eq(0)
     end
   end
 end
