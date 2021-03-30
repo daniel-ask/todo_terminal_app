87249b1ebf2665350f0ee8c4f467a70bb4fbc8ea
Daniel <dask@CA-EDU01.local>
2021-03-30 12:16:37 +1100

    Added a bunch of more stuff

--- lib/app.rb
+++ lib/app.rb
@@ -5,6 +5,63 @@ class App
     @tasks = []
   end
 
+  def run
+    loop do
+      system 'clear'
+      display_welcome
+      puts '-' * 20
+      display_tasks
+      puts '-' * 20
+      display_menu
+      puts '-' * 20
+      process_menu(menu_input)
+    end
+  end
+
+  def process_menu(menu_choice)
+    case menu_choice
+    when 1
+      display_new_task
+      add_task(task_add)
+    when 2
+      edit_task
+    when 3
+      delete_task_action
+    when 4
+      # toggle a task complete
+      toggle_task_action
+    when 5
+      exit
+    end
+  end
+
+  def edit_task
+    index = select_task_action
+    display_new_task
+    change_task(task_add, index)
+  end
+
+  def delete_task_action
+    index = select_task_action
+    delete_task(index)
+  end
+
+  def toggle_task_action
+    index = select_task_action
+    toggle_complete(index)
+  end
+  
+  def select_task_action
+    display_select_task
+    index = select_task
+    raise StandardError, 'Task is not within range' if index.negative? || index > @tasks.length
+
+    index
+  rescue StandardError
+    puts "Task can't be found"
+    retry
+  end
+
   def add_task(task_input)
     @tasks << { task: task_input, completed: false }
   end
@@ -38,6 +95,23 @@ class App
 
   def display_select_task
     puts 'Please enter the number of the task:'
+  end
+
+  def display_menu
+    puts 'Menu:'
+    puts '1. Add task'
+    puts '2. Edit task'
+    puts '3. Delete task'
+    puts '4. Toggle completed'
+    puts '5. Exit'
+  end
+
+  def display_welcome
+    puts 'WELCOME TO MY APP!'
+  end
+
+  def menu_input
+    gets.to_i
   end
 
   def select_task
